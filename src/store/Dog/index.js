df02fc824e6ef10d0c41df5d58c8e41d339eedfb
Galvesmash <galvesmash.dev@gmail.com>
2019-07-11 17:50:12 -0300

    Update index.js

--- src/store/Dog/index.js
+++ src/store/Dog/index.js
@@ -2,16 +2,7 @@ import getters from './getters'
 import actions from './actions'
 import mutations from './mutations'
 
-const state = () => ({
-  roleId: 1,
-  bookSession: false,
-  bookEvent: false,
-  updateAvailability: false,
-  scheduleEvent: false,
-  makeNewCategory: false,
-  liveEvents: null,
-  enrolledEvents: null
-})
+const state = () => ({})
 
 export default {
   namespaced: true,
